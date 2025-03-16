8a4e39ae861326a59a783e03ecd128905a7f39e7
MasterGragra <chen_alexander93@hotmail.com>
2025-03-16 18:01:12 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.lab52025 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.lab52025 to javafx.fxml;
+    exports com.example.lab52025;
+}

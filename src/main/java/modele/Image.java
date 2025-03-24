d10485e3e0db90ad22e45594e8186f9888eff615
teiki <teiki.shan.1@ens.etsmtl.ca>
2025-03-24 19:28:55 -0400

    pseudocode observer + Image.java

--- src/main/java/modele/Image.java
+++ src/main/java/modele/Image.java
@@ -8,35 +8,53 @@ import java.util.List;
 import javax.imageio.ImageIO;
 import vue.Observer;
 
+/*
+    * Classe représentant une image
+ */
 public class Image implements Subject {
-    private String chemin;
-    private BufferedImage image;
-    private List<Observer> observers = new ArrayList<>();
+    private String chemin; // Chemin du fichier image
+    private BufferedImage image; // Image chargée
+    private List<Observer> observers = new ArrayList<>(); // Liste des observateurs
 
     public Image() {
         // Constructeur par défaut
     }
 
+    /*
+        * Charge une image à partir d'un fichier
+     */
     public void chargerImage(String chemin) throws IOException {
-        this.chemin = chemin;
-        this.image = ImageIO.read(new File(chemin));
-        notifyObservers();
+        this.chemin = chemin; // Mémorisation du chemin
+        this.image = ImageIO.read(new File(chemin)); // Chargement de l'image
+        notifyObservers(); // Notification des observateurs
     }
 
+    /*
+        * Retourne le chemin du fichier image
+     */
     public BufferedImage getBufferedImage() {
         return image;
     }
 
+    /*
+        * Ajoute un observateur à la liste
+     */
     @Override
     public void attach(Observer observer) {
         observers.add(observer);
     }
 
+    /*
+        * Retire un observateur de la liste
+     */
     @Override
     public void detach(Observer observer) {
         observers.remove(observer);
     }
 
+    /*
+        * Notifie tous les observateurs
+     */
     @Override
     public void notifyObservers() {
         for (Observer observer : observers) {
--- src/main/java/modele/Subject.java
+++ src/main/java/modele/Subject.java
@@ -2,6 +2,9 @@ package modele;
 
 import vue.Observer;
 
+/*
+    * Interface représentant un sujet observable
+ */
 public interface Subject {
     void attach(Observer observer);
     void detach(Observer observer);

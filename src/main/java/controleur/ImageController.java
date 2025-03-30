b20acbdd8a4844e563602a29acfa26c76c23d55e
zeusda1000 <164581610+zeusda1000@users.noreply.github.com>
2025-03-30 15:48:44 -0400

    ajout du controler d'image

--- src/main/java/controleur/ImageController.java
+++ src/main/java/controleur/ImageController.java
@@ -1,14 +1,40 @@
 package controleur;
 
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
-import java.util.Stack;
+
 
 public class ImageController{
-    @FXML private StackPane panneau2;
-    @FXML private StackPane panneau3;
+    @FXML private StackPane panneau1, panneau2, panneau3;
+    @FXML private ImageView imageComplete, image2, image3;
 
+    // Stratégies
+    private final ManipulationImageStrategie controleZoom = new ControleZoom();
+    private final ManipulationImageStrategie controleTranslation = new ControleTranslation();
+
+
+    @FXML private void initialize() {
+        setImageControls(panneau2, image2);
+        setImageControls(panneau3, image3);
+    }
+
+    private void setImageControls(StackPane panneau, ImageView image){
+
+        panneau.setOnScroll(event -> {
+            controleZoom.handleEvent(event, image);
+        });
+
+        panneau.setOnMousePressed(event -> {
+            controleTranslation.handleEvent(event,image);
+        });
+
+        panneau.setOnMouseDragged(event -> {
+            controleTranslation.handleEvent(event,image);
+        });
+
+    }
 
 
 

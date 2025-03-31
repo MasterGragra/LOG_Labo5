a76a6f6646486b3b35b3c9880672de86c3902657
zeusda1000 <164581610+zeusda1000@users.noreply.github.com>
2025-03-30 22:34:48 -0400

    Ajout d'une méthode pour que les image dépasse pas le panel

--- src/main/java/controleur/ImageController.java
+++ src/main/java/controleur/ImageController.java
@@ -3,7 +3,7 @@ package controleur;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
-
+import javafx.scene.shape.Rectangle;
 
 
 public class ImageController{
@@ -18,6 +18,7 @@ public class ImageController{
     @FXML
     public void initialize() {
         System.out.println("ImageController initialized");
+
         setImageControls(panneau2, image2);
         setImageControls(panneau3, image3);
     }
@@ -26,6 +27,7 @@ public class ImageController{
 
         panneau.setPickOnBounds(true);
         image.setPickOnBounds(true);
+        clipImageToPanel(panneau);
 
         panneau.setOnScroll(event -> {
             controleZoom.handleEvent(event, image);
@@ -44,6 +46,19 @@ public class ImageController{
         panneau.setOnMouseEntered(e -> System.out.println("Mouse entered pane"));
         panneau.setOnMouseExited(e -> System.out.println("Mouse exited pane"));
 
+    }
+
+    /**
+     * Méthode pour mettre un cadre virutel autour du panel ainsi l'image
+     * dépasse pas le panel
+     * @param panneau
+     */
+    private void clipImageToPanel(StackPane panneau){
+
+        Rectangle clip1 = new Rectangle();
+        clip1.widthProperty().bind(panneau.widthProperty());
+        clip1.heightProperty().bind(panneau.heightProperty());
+        panneau.setClip(clip1);
     }
 
 
